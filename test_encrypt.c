c779c76bf4e1837be94b4d7a9732b59575e51d3f
marshad35 <77304257+marshad35@users.noreply.github.com>
2023-02-19 10:26:12 -0700

    Update test_encrypt.c

--- test_encrypt.c
+++ test_encrypt.c
@@ -1,19 +1,4 @@
-/* Muzammil arshad
-  2       1616513
-  3          muzammil
-  4             B1
-  5                Ildar Akhmetov
-  6                   H03
-  7                      CMPUT 201 Student Submission License Version 2.0
-  8                      Copyright 2021 <Muzammil Arshad>
-  9                      Unauthorized redistribution is forbidden under all circumstances. Use of this software without explicit authorization from the author and the CMPUT 201 Instructor is prohibited.
- 10                      This software was produced as a solution for an assignment or lab in the course CMPUT 201 - Practical Programming Methodology at the University of Alberta, Canada. This solution is confidential and remains confidential after it is submitted for grading. The course staff has the right to run plagiarism-detection too    ls on any code developed under this license, even beyond the duration of the course.
- 11                      Copying any part of this solution without including this copyright notice is illegal.
- 12                      If any portion of this software is included in a solution submitted for grading at an educational institution, the submitter will be subject to the plagiarism sanctions at that institution.
- 13                      This software cannot be publicly posted under any circumstances, whether by the original student or by a third party. If this software is found in any public website or public repository, the person finding it is kindly requested to immediately report, including the URL or other repository locating information, to     the following email addresses:
- 14                            nadi@ualberta.ca
- 15                                  ildar@ualberta.ca */
- 16
+
  17
  18
  19
